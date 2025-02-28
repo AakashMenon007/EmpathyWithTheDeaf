7cb015e5dbae384df1d12d48d42b2292f816a5f4
SURYA VEERA <2417657@Swansea.ac.uk>
2025-02-28 20:58:12 +0000

    Animation to the NPC - fix, airtable questions added, canvas display for interactions.

--- Assets/Scripts/SceneHouseDog.cs
+++ Assets/Scripts/SceneHouseDog.cs
@@ -6,23 +6,32 @@ public class SceneHouseDog : MonoBehaviour
 {
     public GameObject dogObject; // Assign your Dog GameObject in the Inspector
     public GameObject spawnPoint;
+    public GameObject NPC;
 
     public OVRScreenFade fade;
 
+    private bool flag = false;          //assigns the flag to false, for scene change condition
+
     private void Start()
     {
-        if (dogObject != null)
+        if (dogObject != null && NPC != null)
         {
             //
             dogObject.SetActive(false); // Ensure dog starts as inactive
+            NPC.SetActive(false);   
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) // Ensure the player has the "Player" tag
+        if (other.CompareTag("Player") && flag == false) // condition tag player and flag check
         {
             StartCoroutine(MovePlayer());
+        }
+        else
+        {
+            //Scene change to AIRTABLE scene
+            Debug.Log("Player has already entered the trigger");
         }
     }
     public IEnumerator MovePlayer()
@@ -33,11 +42,13 @@ public class SceneHouseDog : MonoBehaviour
         transform.position = spawnPoint.transform.position;     // Move the player to the spawn point after scene change
         Debug.Log($"Player moved to position: {spawnPoint.transform.position}");
 
-        if (dogObject != null)
+        if (dogObject != null && NPC != null)
         {
             dogObject.SetActive(true); // Activate the dog when the player enters the trigger
+            NPC.SetActive(true);
         }
 
         fade.FadeIn();
+        flag = true; // Set the flag to true to prevent the scene change from happening again
     }
 }
